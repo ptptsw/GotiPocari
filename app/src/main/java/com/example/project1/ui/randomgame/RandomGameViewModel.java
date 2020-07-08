693d1da19401f7f2b19b0ad99972b78952e76a0c
pacokwon <haechank@gmail.com>
2020-07-08 22:28:14 +0900

    remove: redundant template code from randomgame fragment

--- app/src/main/java/com/example/project1/ui/randomgame/RandomGameViewModel.java
+++ app/src/main/java/com/example/project1/ui/randomgame/RandomGameViewModel.java
@@ -5,15 +5,6 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class RandomGameViewModel extends ViewModel {
-
-    private MutableLiveData<String> mText;
-
     public RandomGameViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is Random Game fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
     }
 }
