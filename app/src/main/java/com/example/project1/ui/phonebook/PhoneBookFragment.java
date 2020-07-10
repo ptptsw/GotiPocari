199ab472d18ded270f791633969ab1d5dc731344
pacokwon <haechank@gmail.com>
2020-07-10 14:16:52 +0900

    remove: redundant imports

--- app/src/main/java/com/example/project1/ui/phonebook/PhoneBookFragment.java
+++ app/src/main/java/com/example/project1/ui/phonebook/PhoneBookFragment.java
@@ -1,9 +1,6 @@
 package com.example.project1.ui.phonebook;
 
-import android.app.SearchManager;
-import android.content.Context;
 import android.database.Cursor;
-import android.graphics.Movie;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -14,21 +11,13 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.SearchView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.project1.R;
 import com.example.project1.ui.search.SearchAdapter;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
