2a96deb23fbb3e8a3e71ecfcee5f825fd906bd44
pacokwon <haechank@gmail.com>
2020-07-14 16:02:58 +0900

    add: GamePagerFragment class

---
+++ app/src/main/java/com/example/project1/ui/gamepager/GamePagerFragment.java
@@ -0,0 +1,41 @@
+package com.example.project1.ui.gamepager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.project1.R;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class GamePagerFragment extends Fragment {
+    private ViewPager2 viewPager;
+    private FragmentStateAdapter pagerAdapter;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_screen_slide, container, false);
+
+        pagerAdapter = new GamePagerAdapter(getActivity());
+        viewPager = root.findViewById(R.id.game_pager);
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setPageTransformer(new ZoomOutPageTransformer());
+
+        TabLayout tabLayout = root.findViewById(R.id.tab_layout);
+        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.OnConfigureTabCallback() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(((GamePagerAdapter)pagerAdapter).getFragmentName(position));
+            }
+        }).attach();
+
+        return root;
+    }
+
+
+}
